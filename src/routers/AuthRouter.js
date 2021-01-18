5fcbdff93dd9e0673c109861eb8a3eb5d1912297
José Agüero <joseaguero.vg@gmail.com>
2021-01-18 14:40:18 -0300

    login & register

--- src/routers/AuthRouter.js
+++ src/routers/AuthRouter.js
@@ -7,14 +7,19 @@ import {
 import { LoginPage } from '../pages/LoginPage';
 import { RegisterPage } from '../pages/RegisterPage';
 
+import logo from '../assets/logo.png';
+
 export const AuthRouter = () => {
     return (
-        <div>
-            <Switch>
-                <Route path="/auth/login" component={ LoginPage } />
-                <Route path="/auth/register" component={ RegisterPage } />
-                <Redirect to="/auth/login" />
-            </Switch>
+        <div className="auth__content">
+            <div className="auth__center-box">
+                <img src={logo} className="auth__logo" alt={'Chat ReactJS'} />
+                <Switch>
+                    <Route path="/auth/login" component={ LoginPage } />
+                    <Route path="/auth/register" component={ RegisterPage } />
+                    <Redirect to="/auth/login" />
+                </Switch>
+            </div>
         </div>
     )
 }
--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -7,7 +7,6 @@ export const PublicRoute = ({
     component: Component,
     ...rest
 }) => {
-
     return (
         <Route {...rest} component={(props) => (
             (isAuthenticated)
